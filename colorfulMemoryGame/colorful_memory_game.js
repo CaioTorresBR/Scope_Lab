c8440bbda5a9d2d91a612fc3a1396979a4c55a07
Caio <caiotorresbr@gmail.com>
2024-07-04 13:15:43 -0400

    message

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -13,7 +13,7 @@ let selectedCards = [];
 // The score gets incremented whenever the player matches a pair of cards successfully
 let score = 0;
 
-// represents the time remaining for the player to complete the game
+// represents time remaining for player to complete the game
 let timeLeft = 30;
 
 // control the countdown mechanism for the game's duration. 
@@ -80,6 +80,7 @@ function handleCardClick(event) {
         // sets the text content of the clicked card to the value stored in it's 'dataset.color'
         // reveals the card's color by changing the text content to the color value
     card.textContent = card.dataset.color;
+
         // changes the card background color to match the revealed color
     card.style.backgroundColor = card.dataset.color;
 
@@ -96,3 +97,108 @@ function handleCardClick(event) {
         setTimeout(checkMatch, 500);
     }
 }
+
+
+// evaluates whether the two selected cards match each other in the memory
+// match game 
+function checkMatch() {
+
+    // uses array destructuring to assign the first two elements of the 'selectedCards' to 'card1' and 'card2'
+    const [card1, card2] = selectedCards;
+
+    // comparing  cards colors:
+        // checks if color value stored in 'dataset.color' attribute of 'card1'
+        // matches color value of 'card2'
+    if (card1.dataset.color === card2.dataset.color) {
+        // if matched, adds class 'matched' to both cards
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+
+        // increases score by 2 pts when player matches a pair
+        score += 2;
+
+        // shows updated score
+        scoreElement.textContent = `Score: ${score}`;
+
+    } else {
+        // if two cards selected don't match, it resets the text content
+        // of both cards to '?', hiding the their colors again
+        card1.textContent = '?';
+        card2.textContent = '?';
+
+        // sets background color to default (visual cue that cards didn't match)
+        card1.style.backgroundColor = '#ddd';
+        card2.style.backgroundColor = '#ddd';
+    }
+        // clears the array to reset it for the next set of card selections
+    selectedCards = [];
+}
+
+
+// initializes and starts game
+function startGame() {
+    // initializes timeLeft to 30 seconds
+    let timeLeft = 30;
+
+    // disables 'startbtn' button to prevent multiple game initiations simultaneously  
+    startbtn.disabled = true;
+
+    score = 0; // Reset score to zero
+
+    // updates displayed score to 0
+    scoreElement.textContent = `Score: ${score}`;
+
+    // initiates game timer counting down from 'timeLeft' duration
+    startGameTimer(timeLeft);
+
+
+    // preparing cards and game elements :
+        // shuffles the 'colors' array and duplicates it to create pairs
+    cards = shuffle(colors.concat(colors));
+    
+        // clears selectedCards array, t prepare for new card selections in the new game
+    selectedCards = [];
+
+        // clears gameContainer, removing cards from previous games
+    gameContainer.innerHTML = '';
+
+        // generates new set of cards within the game container
+    generateCards();
+
+    // adds event listener to the game container
+    // enabling card clicks and triggering the 'handleCardClick' function
+    // to manage the gameplay when cards are clicked
+    gameContainer.addEventListener('click', handleCardClick);
+}
+
+
+// manages game timer
+function startGameTimer(timeLeft) {
+    //sets initial display of the timer
+    timerElement.textContent = `Time Left: ${timeLeft}`;
+
+    // initiates an intervale that triggers a function every second (1000 miliseconds)
+    // to update the timer
+    gameInterval = setInterval(() => {
+        // decrements timeLeft every second within the interval
+        timeLeft--;
+
+        // updates displayed time after each decrement
+        timerElement.textContent = `Time Left: ${timeLeft}`;
+
+        // checks if remaining time reaches 0
+        if (timeLeft === 0) {
+            // stops interval, ending timer from counting down further
+            clearInterval(gameInterval);
+
+            let timeLeft = 30;
+            alert('Game Over!');
+
+            // re-enables 'startbtn' button, allowing player to start a new game
+            startbtn.disabled = false;
+        }
+    }, 1000);
+    }
+
+// triggers startGame function 
+startbtn.addEventListener('click', startGame);
